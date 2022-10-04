356bb8bb4ef054bbbec8bcfa05abdf258567abdf
Nirmaan Sonatun <nsonatun@hotmail.com>
2022-10-04 21:45:52 +0400

    Added validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,9 @@
 class Car < ApplicationRecord
+  has_many :bookings, dependent: :destroy
   belongs_to :user
+
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :address, presence: true
+  validates :price_per_day, presence: true, numericality: { greater_than: 0 }
 end
