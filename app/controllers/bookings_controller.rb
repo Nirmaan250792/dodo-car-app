04c5deec55b9bd0b5210d4494a24bbf949fe4ca0
Nirmaan Sonatun <nsonatun@hotmail.com>
2022-10-05 15:23:33 +0400

    Done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,31 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: %i[show edit update destroy]
-  before_action :set_car, only: %i[show]
-  before_action :set_user, except: %i[index show]
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+  before_action :set_car, only: [:show, :create]
+  before_action :set_user, except: [:index, :show]
 
   def new
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new
   end
+
+  # def create
+  #   @booking = Booking.new(booking_params)
+  #   if @booking.save
+  #     redirect_to car_path(@car), notice: 'Booking was successfully created.'
+  #   else
+  #     # @bookings = @booking.car
+  #     # render "cars/show", notice: 'Booking was not created. Please fill fields with valid information.'
+  #     render :new, notice: 'Booking was not created. Please fill fields with valid information.'
+  #   end
+  # end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.car = @car
-    @booking.id = @booking.id
-    @booking.start_date = @booking.start_date
-    @booking.end_date = @booking.end_date
+    @booking.user = current_user
     if @booking.save
-      redirect_to car_path(@car), notice: 'Booking was successfully created.'
+      redirect_to car_path(@car), notice: "Booking was successfully created"
     else
-      render :new, notice: 'Booking was not created. Please fill fields with valid information.'
+      render 'cars/show', notice: "Booking was not created. Please fill fields with valid information."
     end
   end
 
@@ -27,7 +36,7 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :car_id)
+    params.require(:booking).permit(:start_date, :end_date, :car_id, :is_accepted)
   end
 
   def set_car
