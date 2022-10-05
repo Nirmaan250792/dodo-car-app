72032e55c18ee60e2ebab4445ac270eb0331b0e4
Nirmaan Sonatun <nsonatun@hotmail.com>
2022-10-05 07:40:46 +0400

    Nearly finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require "open-uri"
-
 puts "cleaning up database"
 
 Booking.destroy_all
